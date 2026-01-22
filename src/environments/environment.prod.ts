71376a6ab3040ac0631ff0850905ff2ea0933fed
Shubham Nandeshwar <shubham.nandeshwar94@gmail.com>
2026-01-22 10:28:11 +0530

    Updated env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl:  process.env['NG_APP_API_BASE_URL'],
-  uiBaseUrl:  process.env['NG_APP_UI_BASE_URL']
+  apiBaseUrl:  process.env['NG_APP_API_BASE_URL'] || '',
+  uiBaseUrl:  process.env['NG_APP_UI_BASE_URL'] || ''
 };
 
