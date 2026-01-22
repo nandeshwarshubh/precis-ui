a3688c4395aa112d5c05440c3f244752b62be835
Shubham Nandeshwar <shubham.nandeshwar94@gmail.com>
2026-01-22 10:21:45 +0530

    Environment variable updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:8080/app/rest',
-  uiBaseUrl: 'http://localhost:4200',
+  apiBaseUrl:  process.env['NG_APP_API_BASE_URL'],
+  uiBaseUrl:  process.env['NG_APP_UI_BASE_URL']
 };
 
