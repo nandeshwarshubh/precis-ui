19cd717374141a6eb84e6c3633c7e120b6342153
Shubham Nandeshwar <shubham.nandeshwar94@gmail.com>
2026-01-22 11:42:13 +0530

    Updated env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://precis-rest.onrender.com/app/rest',
-  uiBaseUrl: 'https://precis-ui-ng.vercel.app/'
+  uiBaseUrl: 'https://precis-ui-ng.vercel.app'
 };
 
